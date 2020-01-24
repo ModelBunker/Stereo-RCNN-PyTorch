59478c24c3593b23ff6324e8189b72755ccc5702
fgabel <kpazrael2@gmail.com>
2020-01-24 20:21:27 +0300

    fresh commit

---
+++ todo.sh
@@ -0,0 +1,12 @@
+only works with scipy 1.2.1
+
+pretrianed weights müssen in folder models_stereo
+
+python demo.py
+
+checkout 1.0
+
+build mit python setup.py build
+python steup.py develop
+
+env_stereo
